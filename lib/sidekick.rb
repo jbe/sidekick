1ba685f3f32e9ff31e242d08ec6fec23f7fc195e
jbe <post@jostein.be>
2010-10-30 16:26:18 +0200

    literate documentation

--- lib/sidekick.rb
+++ lib/sidekick.rb
@@ -1,8 +1,27 @@
+# *Sidekick* is a simple event driven background assistant. Among other things, you can use it to automatically compile assets, test code, restart servers and so on - as prescribed per project, in a `.sidekick` file. It is powered by EventMachine and Tilt.
+#
+# This is the annotated source code. See the [README](http://github.com/jbe/sidekick#readme) too.
+#
+# ***
+#
+# Sidekick basically helps you do two things:
+#
+# -- *Define* named triggers, such as saying that `watch(glob)` means doing something when a file matching `glob` changes, or that `every(duration)` means doing something every `duration` seconds.
+#
+# -- *Use* the defined triggers with callbacks, such as `watch(**.rb) { notify 'Code change' }`
+
 require 'fileutils'
 require 'eventmachine'
 
 module Sidekick
 
+  # This core functionality is provided by `Sidekick::Triggers`.
+  #
+  # New triggers can be defined by calling `Sidekick::Triggers.register(:trigger_name) { ... }`.
+  #
+  #Basically, the job of a trigger definition is to take the parameters and the block from a call in `.sidekick` and use it to hook into EventMachine in some way. -- Just have a look at the [default trigger library](http://github.com/jbe/sidekick/blob/master/lib/sidekick/triggers.rb).
+  #
+  # By using Ruby's `method_missing`, we can forward method calls to the registered trigger definitions. Any module can thereby extend the `Triggers` module in order to expose the defined triggers as if they were methods.
   module Triggers
     @@triggers = {}
 
@@ -25,15 +44,28 @@ module Sidekick
 
   end
 
-  # default library
+  # A default library of triggers and helpers is included..
   require 'sidekick/triggers'
   require 'sidekick/helpers'
 
-  # context in which to evaluate .sidekick file
+  # The `.sidekick` file is evaluated in a `Sidekick::Context` module, which exposes DSL style methods by extending `Sidekick::Triggers` and `Sidekick::Helpers`.
   Context = Module.new
   Context.extend Triggers
   Context.extend Helpers
 
+
+
+  # The `Sidekick.run!` method reads and applies the `.sidekick` file, wrapping the setup phase inside `EM.run { .. }`, and thus begins the event loop.
+  def self.run!(path='.sidekick')
+    ensure_config_exists(path)
+
+    Signal.trap(:INT) { stop }
+
+    EventMachine.run do
+      Context.module_eval(
+        open(path) {|f| f.read }, path )
+    end
+  end
 
   def self.ensure_config_exists(path)
     unless File.exists?(path)
@@ -44,25 +76,9 @@ module Sidekick
     end
   end
 
-  # reads and applies the .sidekick file, and begins
-  # the event loop.
-  def self.run!(path='.sidekick')
-
-    ensure_config_exists(path)
-
-    Signal.trap(:INT) { ::Sidekick.stop }
-
-    EventMachine.run do
-      Context.module_eval(
-        open(path) {|f| f.read }, path )
-    end
-  end
-
-  # stops Sidekick gracefully
   def self.stop(msg=false)
     EventMachine.stop
-    puts
-    puts msg if msg
+    puts "\n#{msg}" if msg
   end
 
 end
